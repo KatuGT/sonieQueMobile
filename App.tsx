7bc384095d5fded0b2f1eb83203da5a536a3e5fd
katu <93katu@gmail.com>
2023-11-03 23:45:35 -0300

    agregado TS, native wind y alias

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
+
+export default function App() {
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
